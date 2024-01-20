0c0b0c98d4df7b94a3dbd2c35ddbc19399098a8f
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-01-20 23:27:41 +0530

    add binding

---
+++ app/src/main/java/com/bhaskardamayanthi/resoluteassientment/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bhaskardamayanthi.resoluteassientment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bhaskardamayanthi.resoluteassientment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
